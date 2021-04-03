1ebd7b3485e95b119125be70063ed4c81f068d3c
sakai-akinobu <sakai.akinobu@gmail.com>
2021-04-03 18:32:37 +0900

    SSR with React.

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,11 @@
 import express from 'express';
+import {renderToString} from 'react-dom/server';
 
 const app = express();
 
-app.get('/', (_, res) => res.send('Hello World'));
+app.get('/', (_, res) => {
+  res.send(renderToString(<div>Hello World</div>));
+});
 
 const PORT = 3000;
 app.listen(PORT, () => console.log('Server started.'));
