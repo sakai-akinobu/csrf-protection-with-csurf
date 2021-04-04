d68453d361df5ea58177c06c64353478158b353a
sakai-akinobu <sakai.akinobu@gmail.com>
2021-04-04 13:24:24 +0900

    Add request patterns.

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,23 +1,65 @@
 import {useState, useEffect} from 'react';
 
 export const App: React.FC = () => {
+  const [cookie, setCookie] = useState('');
   const [csrfToken, setCsrfToken] = useState('');
 
   useEffect(() => {
+    setCookie(document.cookie);
     setCsrfToken(document.querySelector('meta[name="csrf-token"]')?.getAttribute('content') || '');
   }, []);
 
-  const onClick: React.MouseEventHandler = async () => {
-    await fetch('/api', {
-      method: 'POST',
-      headers: {'csrf-token': csrfToken},
-      credentials: 'same-origin',
-    });
+  const [fetchOptions, setFetchOptions] = useState<RequestInit>();
+  const [response, setResponse] = useState<Readonly<{status: number, statusText: string}>>();
+
+  const createEventHandler = (overwriteOptions: Omit<RequestInfo, 'headers' | 'credentials'> = {}) => {
+    return async() => {
+      const options: RequestInit =  {
+        method: 'POST',
+        headers: {'csrf-token': csrfToken},
+        credentials: 'same-origin',
+        ...overwriteOptions,
+      };
+
+      const {status, statusText} = await fetch('/api', options);
+
+      setFetchOptions(options);
+      setResponse({
+        status,
+        statusText,
+      });
+    };
   };
 
   return (
     <div>
-      <button onClick={onClick}>submit</button>
+      <button onClick={createEventHandler()}>
+        Valid request
+      </button>
+      <button onClick={createEventHandler({headers: {'csrf-token': 'invalid token'}})}>
+        Invalid CSRF-Token
+      </button>
+      <button onClick={createEventHandler({credentials: 'omit'})}>
+        Missing token secret
+      </button>
+      {cookie &&
+        <div>
+          <h4>Cookie</h4>
+          <pre>{cookie}</pre>
+        </div>
+      }
+      {fetchOptions &&
+        <>
+          <h4>Fetch options</h4>
+          <pre>{JSON.stringify(fetchOptions, null, 2)}</pre>
+        </>
+      }
+      {response &&
+        <>
+          <h4>Response</h4>
+          <pre>{JSON.stringify(response, null, 2)}</pre>
+        </>
+      }
     </div>
   );
 };
